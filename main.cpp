1fce53d7a1f8f140a59f95e6bdcc5ca51a21a7b4
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-10-20 15:17:14 -0500

    Refactor matrix math functions

--- main.cpp
+++ main.cpp
@@ -1,20 +1,18 @@
 #include <fstream>
 #include <iostream>
 
-const int SIZE = 100;
+const int MAX_SIZE = 100;
 
-// function prototypes
-void read_matrix(int first_matrix[SIZE][SIZE], int second_matrix[SIZE][SIZE],
-                 int size, const std::string &filename);
-void print_matrix();
-void add_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]);
-void subtract_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]);
-void multiply_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]);
+void read_matrix(int first_matrix[MAX_SIZE][MAX_SIZE], int second_matrix[MAX_SIZE][MAX_SIZE], int &size, const std::string &filename);
+void print_matrix(int matrix[MAX_SIZE][MAX_SIZE], int &size);
+void add_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]);
+void subtract_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]);
+void multiply_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]);
 
 int main() {
-  int first_matrix[SIZE][SIZE];
-  int second_matrix[SIZE][SIZE];
-  int result_matrix[SIZE][SIZE];
+  int first_matrix[MAX_SIZE][MAX_SIZE];
+  int second_matrix[MAX_SIZE][MAX_SIZE];
+  int result_matrix[MAX_SIZE][MAX_SIZE];
   int size;
 
   read_matrix(first_matrix, second_matrix, size, "input");
@@ -45,31 +43,30 @@ void read_matrix(int first_matrix[SIZE][SIZE], int second_matrix[SIZE][SIZE],
 
   File.close();
 }
-// functions go here
-void add_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]) {
+void add_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-        result[i][j] = matrix1[i][j] + matrix2[i][j];
+      result[i][j] = matrixA[i][j] + matrixB[i][j];
     }
   }
 }
 
-void subtract_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]) {
+void subtract_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-        result[i][j] = matrix1[i][j] - matrix2[i][j];
+      result[i][j] = matrixA[i][j] - matrixB[i][j];
     }
   }
 }
 
-void multiply_matrices(int matrix1[SIZE][SIZE], int matrix2[SIZE][SIZE], int &size, int result[SIZE][SIZE]) {
+void multiply_matrices(int matrixA[MAX_SIZE][MAX_SIZE], int matrixB[MAX_SIZE][MAX_SIZE], int &size, int result[MAX_SIZE][MAX_SIZE]) {
   for (int i = 0; i < size; i++) {
     for (int j = 0; j < size; j++) {
-        int dot_product = 0;
-        for (int k = 0; k < size; k++) {
-            dot_product += matrix1[i][k] * matrix2[k][j];
-        }
-        result[i][j] = dot_product;
+      int dot_product = 0;
+      for (int k = 0; k < size; k++) {
+        dot_product += matrixA[i][k] * matrixB[k][j];
+      }
+      result[i][j] = dot_product;
     }
   }
 }
